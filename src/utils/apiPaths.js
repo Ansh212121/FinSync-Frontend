2107cc4bfd172a5dfd6fefbc0d6ac7e3e50139a8
Ansh212121 <ansh2109ag@gmail.com>
2025-06-04 18:00:11 +0530

    fixed

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://transmart-backend.onrender.com"; 
+export const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 export const API_PATHS = {
     AUTH: {
